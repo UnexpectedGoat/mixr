dfadbc9f716b80cf77d4333eba97db7ad1a4f6a9
Asmar <asmarcd@gmail.com>
2020-10-19 14:08:08 -0700

    controller / route complete. index page loads at localhost:3000

--- controllers/mixr_controller.js
+++ controllers/mixr_controller.js
@@ -5,10 +5,37 @@ var router = express.Router();
 
 var db = require("../models");
 
+const apiTest = {
+  "idDrink": "11007",
+  "strDrink": "Margarita",
+  "strTags": "IBA,ContemporaryClassic",
+  "strCategory": "Ordinary Drink",
+  "strIBA": "Contemporary Classics",
+  "strAlcoholic": "Alcoholic",
+  "strGlass": "Cocktail glass",
+  "strInstructions": "Rub the rim of the glass with the lime slice to make the salt stick to it. Take care to moisten only the outer rim and sprinkle the salt on it. The salt should present to the lips of the imbiber and never mix into the cocktail. Shake the other ingredients with ice, then carefully pour into the glass.",
+  "strDrinkThumb": "https://www.thecocktaildb.com/images/media/drink/5noda61589575158.jpg",
+  "strIngredient1": "Tequila",
+  "strIngredient2": "Triple sec",
+  "strIngredient3": "Lime juice",
+  "strIngredient4": "Salt",
+  "strMeasure1": "1 1/2 oz ",
+  "strMeasure2": "1/2 oz ",
+  "strMeasure3": "1 oz ",
+  }
+
 // TODO: route for getting drinks in favorites
 // TODO: route for deleting items from pantry
 // TODO: route for creating your own cocktail to favorites
 router.get("/", function(req, res) {
+  // db.all(data => {
+    // Using apiTest variable above for testing. Will swap out with handlebars variable below. as we build things out.
+    // const hbsObject = {
+    //   drinks: data
+    // };
+    // res.render("index", hbsObject)
+  // });
+  res.render("index", apiTest)
 });
   // TODO: get cocktails from cocktailDB with axios call
 router.post("/cocktails/create", function(req, res) {
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var db = require("./models")
 var app = express();
-var routes = require("./controllers/burgers_controller.js");
+var routes = require("./controllers/mixr_controller.js");
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
@@ -9,21 +9,16 @@ app.use(express.json());
 app.use(routes);
 
 //handlebars
-const handlebars = require("express-handlebars");
+const exphbs = require("express-handlebars");
 
-app.engine(handlebars({
-  defaultLayout: "index",
-  layoutsDir: `$(_dirname)/views/layouts`,
-  partialsDir: `$(_dirname)/views/partials`
-}));
+app.engine("handlebars", exphbs({defaultLayout: "main",}));
+app.set("view engine", "handlebars");
+
 app.use(express.static('public'));
-app.get("/", (req, res) => {
-  res.render('main', {layout: 'index'});
-
 
 var PORT = process.env.PORT || 3000;
-db.sequelize.sync({force:true}).then(function() {
-  app.listen(PORT, function() {
+db.sequelize.sync({ force: true }).then(function () {
+  app.listen(PORT, function () {
     console.log("App now listening on port:", PORT);
   });
-})})
+});
