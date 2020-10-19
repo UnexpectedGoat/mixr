e25d33a9d463ccb2c5dc0f6d63fd895a5387d06e
Hilary Valencia-Walsh <hilaryvalenciawalsh@gmail.com>
2020-10-18 19:43:51 -0700

    put in code for server.js handlebars

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require("express");
 var db = require("./models")
 var app = express();
+var routes = require("./controllers/burgers_controller.js");
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(routes);
 
 //handlebars
 const handlebars = require("express-handlebars");
@@ -12,12 +14,11 @@ const handlebars = require("express-handlebars");
 app.engine(handlebars({
   defaultLayout: "index",
   layoutsDir: `$(_dirname)/views/layouts`,
+  partialsDir: `$(_dirname)/views/partials`
 }));
-app.set("view engine", "handlebars");
-
-var routes = require("./controllers/burgers_controller.js");
-
-app.use(routes);
+app.use(express.static('public'));
+app.get("/", (req, res) => {
+  res.render('main', {layout: 'index'});
 
 
 var PORT = process.env.PORT || 3000;
@@ -25,4 +26,4 @@ db.sequelize.sync({force:true}).then(function() {
   app.listen(PORT, function() {
     console.log("App now listening on port:", PORT);
   });
-});
+})})
