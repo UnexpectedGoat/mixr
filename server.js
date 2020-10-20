7a9352ceb736359a7e1237a7a9fcf631435c5f36
Jonny K <Kempj2.jk@gmail.com>
2020-10-19 21:51:49 -0700

    startings of a cocktail creation form with upload

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ var express = require("express");
 var db = require("./models")
 var app = express();
 var routes = require("./controllers/mixr_controller.js");
+var path = require('path');
+
 
 app.use(express.static("public"));
+app.use('/scripts', express.static(path.join(__dirname, 'node_modules')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
