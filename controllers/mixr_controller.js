21fff6b6de88d39da42509264a355c1ebf4a698f
Jonny K <Kempj2.jk@gmail.com>
2020-10-21 18:44:25 -0700

    code commented and ready to rip

--- controllers/mixr_controller.js
+++ controllers/mixr_controller.js
@@ -1,7 +1,5 @@
 var express = require("express");
-
 var router = express.Router();
-
 var db = require("../models");
 
 const axios = require('axios');
@@ -87,34 +85,69 @@ const pantryTest = [{
 // TODO: route for deleting items from pantry
 // TODO: route for creating your own cocktail to favorites
 // ---------------------------------USER AUTH ROUTES ---------------------------------
+//route for delivering login handlebars, if the user is already
+//need to investigate why we send user object, maybe for autofill?
 router.get("/login", (req, res) => {
     res.render("login", { user: req.session.user })
 })
+//route for delivering signup handlebars
 router.get("/signup", (req, res) => {
     res.render("signup", { user: req.session.user })
 })
+//this is the post version of login, so when we send it form data aka login creds do this
 router.post('/login', (req, res) => {
+    // finds one user based on where teh username in body matches that in the db
     db.User.findOne({
         where: { username: req.body.username }
     }).then(user => {
-        //check if user entered password matches db password
+        //this is where the auth magic happens
+        //if no user exists in db
         if (!user) {
+            //destroy the req.session for security and send back error
             req.session.destroy();
             return res.status(401).send('incorrect username or password')
 
-        } else if (bcrypt.compareSync(req.body.password, user.password)) {
+        } 
+        //else if the password in req.body matches the user hash password
+        //the has password is created in the user model, and adds salt to plain test
+        //bcrypt is the package doing the encryping and allowing us to decode
+        else if (bcrypt.compareSync(req.body.password, user.password)) {
+            //if the user credentials are good, set the session equal to such so that
+            //the user can access all the data they are linked to
             req.session.user = {
                 username: user.userame,
                 id: user.id
             }
+            //plop them on their drinks page
             return res.redirect("/drinks")
         }
+        //else the password must have been wrong so reject agaain
         else {
             req.session.destroy();
             return res.status(401).send('incorrect username or password')
         }
     })
 })
+//route for handling the actual signup request
+router.post('/signup', (req, res) => {
+    //create a new user taking in the req.body
+    db.User.create({
+        username: req.body.username,
+        password: req.body.password
+    }).then(newUser => {
+        //take that new user and set their session cookie to that info
+        req.session.user = {
+            username: newUser.username,
+            id: newUser.id
+        }
+        //plop them on their drink page
+        res.redirect("/drinks")
+    }).catch(err => {
+        console.log(err);
+        res.status(500).send("server error")
+    })
+})
+
 // ---------------------------------API Routes ---------------------------------
 // updated the route to drinks
 router.get("/drinks", function (req, res) {
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config()
 var express = require("express");
 var db = require("./models")
 var app = express();
@@ -6,14 +7,16 @@ var path = require('path');
 const seedModels = require("./seedModels.js")
 const session = require('express-session')
 
-require('dotenv').config()
+var PORT = process.env.PORT || 3000;
+
+
 
 
 app.use(express.static("public"));
 app.use('/scripts', express.static(path.join(__dirname, 'node_modules')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(routes);
+
 
 // User session boilerplate, saves the user as logged in as a cookie
 app.use(session({
@@ -24,6 +27,7 @@ app.use(session({
       maxAge: 2 * 60 * 60 * 1000
   }
 }))
+app.use(routes);
 
 //handlebars
 const exphbs = require("express-handlebars");
@@ -32,8 +36,8 @@ app.engine("handlebars", exphbs({defaultLayout: "main",}));
 app.set("view engine", "handlebars");
 
 
-var PORT = process.env.PORT || 3000;
-db.sequelize.sync({ force: true }).then(function () {
+
+db.sequelize.sync({ force: false }).then(function () {
   app.listen(PORT, function () {
     seedModels()
     console.log("App now listening on port:", PORT);
