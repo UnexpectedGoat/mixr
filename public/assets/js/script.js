780bf0a4e35f667ef5e3b9f26dc3e7785635450c
Jonny K <Kempj2.jk@gmail.com>
2020-10-22 22:09:25 -0700

    scripts cleaned up

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -96,6 +96,7 @@ $(".delete-pantry-button").on("click", function (event){
     })
 })
 
+
 // Building the cloudinary widget for uploads on button click below
 var myWidget = cloudinary.createUploadWidget(
   {
@@ -121,17 +122,4 @@ document.getElementById("upload_widget").addEventListener(
   },
   false
 );
-
-// Side Nav bar
-document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('.sidenav');
-    var instance = M.Sidenav.getInstance(elems);
-  });
-  // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
-  // var collapsibleElem = document.querySelector('.collapsible');
-  // var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
-  // Or with jQuery
-  $(document).ready(function () {
-    $('.sidenav').sidenav();
-  });
 
