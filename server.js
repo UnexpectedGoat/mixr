10327d0feebe8784d83c23fa2661073b7f26ec82
Jonny K <Kempj2.jk@gmail.com>
2020-10-24 11:34:20 -0700

    new port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var path = require('path');
 const seedModels = require("./seedModels.js")
 const session = require('express-session')
 
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3002;
 
 
 
